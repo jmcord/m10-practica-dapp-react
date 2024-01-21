c4b725167258d982380e9487be1db99a570ce435
jmcord <jcorderodz@hotmail.com>
2024-01-21 20:59:36 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 export default function Header(){
     return (
-        <header className="flex p-2 bg">
-                <img src="bm.png"/>
+        <header className="flex px-8 py-4 border-b justify-between items-center
+        bg-white">
+                <img src="bm.png" alt="blockmaker-logo" className="sm:hidden" width={50}/>
+                <img src="logo-blockmaker.png" alt="blockmaker-small-logo" className="hidden sm:flex" width={200}/>
+
+                <button className="bg-gray-100 rounded-md">Conectar Wallet</button>
+         
         </header>
     )
 }
